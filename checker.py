3a292da3575a5957eec3f651f0558f0256d83503
Oliver Payne <olipayne@gmail.com>
2016-08-16 15:13:59 +0200

    more questions, and make sure the file exists

--- checker.py
+++ checker.py
@@ -3,6 +3,7 @@
 import urllib2
 from distutils.util import strtobool
 import sys
+import os.path
 
 def user_yes_no_query(question):
     sys.stdout.write('%s [y/n]\n' % question)
@@ -12,6 +13,12 @@ def user_yes_no_query(question):
         except ValueError:
             sys.stdout.write('Please respond with \'y\' or \'n\'.\n')
 
-list_path = raw_input("Please specify the full path of the url list: ")
 
+# Ask questions to define usage
+list_path = raw_input("Please specify the full path of the url list: ")
+if os.path.isfile(list_path) is False:
+	print("This is not a valid file")
+	exit(1)
 follow_redirects = user_yes_no_query("Follow redirects?")
+file_output = user_yes_no_query("Output to good.txt and bad.txt? If not, it will just print to terminal")
+
