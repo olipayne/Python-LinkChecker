f501dcf41e7367655dce3a2189b24d39a9f09547
Oliver Payne <olipayne@gmail.com>
2016-08-16 15:09:06 +0200

    asking some questons

--- checker.py
+++ checker.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python
+
+import urllib2
+from distutils.util import strtobool
+import sys
+
+def user_yes_no_query(question):
+    sys.stdout.write('%s [y/n]\n' % question)
+    while True:
+        try:
+            return strtobool(raw_input().lower())
+        except ValueError:
+            sys.stdout.write('Please respond with \'y\' or \'n\'.\n')
+
+list_path = raw_input("Please specify the full path of the url list: ")
+
+follow_redirects = user_yes_no_query("Follow redirects?")
